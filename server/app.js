4cd2e5ea3e0783c5d9a05371d1c57b0c114ade04
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-12-24 15:28:59 +0200

    update error-handler middleware and import morgan package

--- server/app.js
+++ server/app.js
@@ -1,19 +1,37 @@
 require('dotenv').config()
+require('express-async-errors')
 
 // express
 const express = require('express')
 const app = express()
 
-// database
+// rest of the packages
+const morgan = require('morgan')
+
+// imports
 const connectDB = require('./db/connect')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// package middlewares
+app.use(morgan('tiny'))
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+  res.json({ success: true, msg: 'Ecommerce API' })
+})
+
+// error middleware
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 // port
 const port = process.env.PORT || 3001
-
 // server
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
+    await connectDB(process.env.MONGO_URL) // connect to db
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`)
     })
--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,40 +1,33 @@
-const { statusCodes } = require('http-status-codes')
-const { custom } = require('joi')
-
+const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
-    // set default values
-    statusCode: err.statusCode || statusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || 'Something went wrong, please try again later',
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later',
   }
-
-  // handle validation error
+  // Validation Error
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(',')
-
     customError.statusCode = 400
   }
-
-  // handle duplicate key error
+  // duplicate key error
   if (err.code && err.code === 11000) {
-    customError.message = `Duplicate field value entered for ${Object.keys(
+    customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
     customError.statusCode = 400
   }
-
-  // handle cast error
+  // cast error
   if (err.name === 'CastError') {
-    customError.message = `No item found with id of ${err.value}`
+    customError.msg = `No item found with id : ${err.value}`
     customError.statusCode = 404
   }
 
-  return res.status(customError.statusCode).json({
-    success: false,
-    message: customError.message,
-  })
+  return res
+    .status(customError.statusCode)
+    .json({ success: false, msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
