5c3f737e7135e9d6c61024b18c75faa8355d2519
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-12-24 15:39:17 +0200

    Add user model

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name must be at least 3 characters'],
+    maxlength: [50, 'Name must be at most 50 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    validate: {
+      message: 'Email is invalid',
+      validator: validator.isEmail,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+})
+
+module.exports = mongoose.model('User', userSchema)
